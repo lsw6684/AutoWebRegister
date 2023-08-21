874c6f43d9c026b502dedf5c045f0bf712044703
lsw6684 <lsw6684@hanmail.net>
2023-08-21 18:08:50 +0900

    네이버 구글 제출 전까지

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const config = require('./config');
 
 (async () => {
     const browser = await puppeteer.launch({
-        args: ['--incognito'],
-        headless:false, 
+        headless:false,
         defaultViewport: { width: 1200, height: 800 }
-        }); 
-    const page = await browser.newPage();
-    await page.goto('https://searchadvisor.naver.com/console/site/request/crawl?site=https%3A%2F%2Fallhoneytip.com');
+        });
+        const page = await browser.newPage();
+    
+//    const page = await browser.newPage();
+
 
     // await daum(page);
-  // await naver(page);
-   // await google();
+    await naver(page);
+    //await google(page);
 
 
+   
     
-    // 
 
 
 
@@ -52,25 +53,41 @@ async function naver(page) {
     await page.goto('https://searchadvisor.naver.com/console/site/request/crawl?site=https%3A%2F%2Fallhoneytip.com');
 
 
-
-    // Login
-    await page.type('#id', config.naver.id);
-    await page.type('#pw', config.naver.pw);
-    await page.click('#log\\.login');
     
-   //await page.type('#input-212', config.targetUrl);
-   // const buttonSpanSelector = 'button.v-btn--depressed span.v-btn__content'; // 버튼 내부의 span 선택자
-   // const buttonSpan = await page.$(buttonSpanSelector);
-  //  await buttonSpan.click()    
+    // Login
+    await page.evaluate((id, pw) => {
+        document.querySelector("#id").value = id;
+        document.querySelector("#pw").value = pw;
+    }, config.naver.id, config.naver.pw)
+    
+    await page.click('#log\\.login');
+    await page.waitForSelector('#input-212'); // 요소가 나타날 때까지 대기
 
+    await page.type('#input-212', config.targetUrl);
+
+    await page.waitForSelector('.v-btn__content'); // 요소가 나타날 때까지 대기
+    await page.evaluate(() => {
+        const startButton = document.querySelector('button span.v-btn__content');
+        if (startButton) {
+          startButton.click();
+        }
+      });    
 }
 
 
 
 
-async function google() {
+async function google(page) {
     // 웹으로 이동: Google Search Console
     await page.goto('https://search.google.com/search-console/about?hl=ko');
-    await page.click('#log.login');
-
+    await page.evaluate(() => {
+        const startButton = document.querySelector('a span.RveJvd');
+        if (startButton) {
+          startButton.click();
+        }
+      });
+    await page.waitForSelector('#identifierId')
+    await page.type('#identifierId', config.google.id);
+    // await page.waitForSelector('.VfPpkd-vQzf8d'); // 요소가 나타날 때까지 대기
+    // await page.click('.VfPpkd-vQzf8d');
 }
