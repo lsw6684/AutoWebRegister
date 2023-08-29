e61775c053c2a6f2552b712cedf245be21a1a2df
SeungWon Lee <lsw6684@hanmail.net>
2023-08-30 07:48:27 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+    targetUrl:'',   
+    daum: {
+      id: '',     
+      pw: ''       
+    },
+    naver: {
+      id: '',      
+      pw: '',     
+    },
+    google: {
+      id: '',       
+      pw: '',      
+    }
+};
+
+module.exports = config;
